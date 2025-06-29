548aab7cda5fd528b77844a5fd4c55d6611cbd69
Harada <1a88.19e7@gmail.com>
2025-06-29 09:29:25 +0900

    create conversion

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from createBoard import createBoard
 def main():
     qr_data = readQR()
     board_type, board_height = createBoard(qr_data)
+    print(board_type)
+
 
 if __name__ == "__main__":
     main()
